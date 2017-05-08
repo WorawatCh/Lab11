c9d56286158a6877f8e23faae2750122f93cf3d4
Worawat Chueajedton <worawat.chu@ku.th>
2017-05-08 07:05:56 +0000

    Update file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
 
- end
+root 'static_pages#home'
+
+get '/help', to: 'static_pages#help'
+
+get '/about', to: 'static_pages#about'
+
+get '/contact', to: 'static_pages#contact'
+
+get '/login', to: 'static_pages#login'
+
+end
